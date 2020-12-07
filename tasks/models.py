46e2a6e0aa9d0c3a235c8a15e1f8dbe5484b5865
Amilcar M <amilkarms@outlook.com>
2020-12-07 16:57:15 -0600

    Datetime updated
    TIME_ZONE updated to America/Costa Rica to reflect local time.

--- tasks/models.py
+++ tasks/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 class Task(models.Model):
     title = models.CharField(max_length=200)
     complete = models.BooleanField(default=False)
-    created = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=datetime.datetime.now())
 
     def __str__(self):
         return self.title 
